93b5a29a479d3c1cbabee570de3fbc16085a134a
Sofian Eka Sandra <fianekaid@Server.local>
2020-06-16 11:58:51 +0700

    chang epoe

--- config.php
+++ config.php
@@ -3,9 +3,10 @@ $page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '';
 define('PATH', 'http://192.168.1.2/covid/');
 define('SITE_URL', PATH . 'index.php');
 define('POSITION_URL', PATH . '?page=' . $page);
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.1.3');
 define('DB_USERNAME', 'syamrabu');
 define('DB_PASSWORD', 'syamrabu');
 define('DB_NAME', 'syam_covid');
+define('DB_NAME_2', 'syamrabu');
 
 ?>
