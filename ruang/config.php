e20262a7d59f16ce7bc6cc7a8c8dbd07a4318c61
Sofian Eka Sandra <fianekaid@Server.local>
2020-06-16 11:38:48 +0700

    set for syamrabu server

--- ruang/config.php
+++ ruang/config.php
@@ -1,11 +1,11 @@
 <?php
 $page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '';
-define('PATH', 'http://localhost:8888/covid/ruang/');
+define('PATH', 'http://192.168.1.2/covid/ruang/');
 define('SITE_URL', PATH . 'index.php');
 define('POSITION_URL', PATH . '?page=' . $page);
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'fianroot');
-define('DB_PASSWORD', '');
+define('DB_HOST', '192.168.1.3/phpmyadmin');
+define('DB_USERNAME', 'syamrabu');
+define('DB_PASSWORD', 'syamrabu');
 define('DB_NAME', 'syam_covid');
 define('DB_NAME_2', 'syamrabu');
 
