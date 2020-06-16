460c844e9f23303049da029cd4c7ae598684e30f
Sofian Eka Sandra <fianekaid@Server.local>
2020-06-16 12:23:41 +0700

    dasd

--- ruang/modules/views/template.view.php
+++ ruang/modules/views/template.view.php
@@ -12,7 +12,7 @@ $page = $temp[1];
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
+        <title>Dashboard - Ruangan</title>
         <link href="../resource/dist/css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
