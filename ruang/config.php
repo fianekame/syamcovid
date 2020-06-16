9077ccd6c268adbc827bdc1aea52ad329ba4ed2b
Sofian Eka Sandra <fianekaid@Server.local>
2020-06-16 11:41:20 +0700

    resolve host

--- ruang/config.php
+++ ruang/config.php
@@ -3,7 +3,7 @@ $page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '';
 define('PATH', 'http://192.168.1.2/covid/ruang/');
 define('SITE_URL', PATH . 'index.php');
 define('POSITION_URL', PATH . '?page=' . $page);
-define('DB_HOST', '192.168.1.3/phpmyadmin');
+define('DB_HOST', '192.168.1.3');
 define('DB_USERNAME', 'syamrabu');
 define('DB_PASSWORD', 'syamrabu');
 define('DB_NAME', 'syam_covid');
